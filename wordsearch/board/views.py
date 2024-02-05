4e4714b2784d6d0fde2bac2d3236df8797c3702f
Caden-Hornyak <108236501+Caden-Hornyak@users.noreply.github.com>
2024-02-05 00:11:18 -0800

    basic css and js structure

--- wordsearch/board/views.py
+++ wordsearch/board/views.py
@@ -11,4 +11,4 @@ def create_board(request):
 
     board_obj = word_board.word_board(board_dim, specific_board, board_type)
     board = board_obj.board
-    return render(request, "board/create_board.html", {'board': board})
+    return render(request, "create_board.html", {'board': board})
