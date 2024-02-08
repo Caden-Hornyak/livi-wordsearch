43fd2214219918f59ffbe0917287491aa2503d1c
Caden-Hornyak <108236501+Caden-Hornyak@users.noreply.github.com>
2024-02-07 21:46:16 -0800

    board draw enter and keyboard enter

--- wordsearch/board/static/js/board_funcs.js
+++ wordsearch/board/static/js/board_funcs.js
@@ -2,27 +2,99 @@ let current_word = document.getElementById("current_word");
 let score = document.getElementById("score");
 let wrong_word = document.getElementById("wrong_word");
 let word_dic;
+let mouseDown = false;
+let seen = {};
+let completed_words = {};
+
+var ding_audio = new Audio('C:\Users\19494\Desktop\Coding\Python\WordSearchWeb\wordsearch\board\static\sounds\ding.mp3');
+
 
 document.addEventListener('DOMContentLoaded', function () {
     word_dic = JSON.parse(document.getElementById('wordDefinitions').getAttribute('data-words'));
 });
 
-function letter_press(letter) {
-    console.log(letter);
-    current_word.textContent += letter;
+const boardLetters = document.querySelectorAll('.board_letters');
+
+boardLetters.forEach(letter => {
+    letter.addEventListener('mousedown', handleLetterDown);
+    letter.addEventListener('mouseover', handleLetterHover);
+    letter.addEventListener('mouseup', handleLetterMouseUp);
+});
+
+function handleLetterDown() {
+    if (!mouseDown) {
+        console.log("Mouse down!");
+        mouseDown = true;
+        addLetter.call(this);
+    }
 }
 
-function submit_word() {
-    console.log(current_word.textContent.toLowerCase());
-    if (word_dic.hasOwnProperty(current_word.textContent.toLowerCase())) {
+function handleLetterMouseUp() {
+    console.log("Mouse up!");
+    mouseDown = false;
+
+    for (var key in seen) {
+        seen[key].style.backgroundColor = 'white';
+        delete seen[key];
+    }
+    if (current_word.textContent in completed_words) {
+        wrong_word.textContent = current_word.textContent + " already entered.";
+    } else if (word_dic.hasOwnProperty(current_word.textContent.toLowerCase())) {
         let curr_score = Number(score.textContent);
+        ding_audio.play();
         curr_score += current_word.textContent.length
-        console.log(curr_score);
         score.textContent = curr_score.toString();
+        completed_words[current_word.textContent] =  1;
         wrong_word.textContent = "";
     } else {
-        wrong_word.textContent = current_word.textContent + " is invalid";
+        wrong_word.textContent = current_word.textContent + " is invalid.";
     }
     current_word.textContent = "";
 }
 
+let last_hovered = "";
+function handleLetterHover() {
+    if (last_hovered != this.id) {
+        last_hovered = this.id;
+        console.log("Mouse Hover!", mouseDown);
+        if (mouseDown) {
+            addLetter.call(this);
+        }
+    }
+}
+
+function addLetter() {
+    this.style.backgroundColor = 'lightgreen';
+    id = this.id;
+    let char = "", pos_id = "";
+    for (let i = 0; i < id.length; i++) {
+        if (id.charCodeAt(i) <= 57) {
+            pos_id += id[i];
+        } else {
+            char += id[i];
+        }
+    }
+
+    if (pos_id in seen) {
+        console.log("Im calling mouseUp :)")
+        handleLetterMouseUp()
+        return;
+    } else {
+        seen[pos_id] = this;
+    }
+        
+
+    if (current_word.textContent.length > 0) {
+        current_word.textContent += char.toLowerCase();
+    } else {
+        current_word.textContent += char;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        handleLetterMouseUp();
+    } else {
+        current_word.textContent += event.key;
+    }
+});
--- wordsearch/board/word_board.py
+++ wordsearch/board/word_board.py
@@ -5,12 +5,9 @@ class word_board:
     def __init__(self, board_dim, specific_board=[], board_type="new_board"):
 
         self.word_dic = {}
-        count = 0
-        with open(r'C:\Users\19494\Desktop\Coding\Python\WordSearchWeb\wordsearch\board\words.txt', 'r', encoding="mbcs") as f:
+        with open(r'C:\Users\19494\Desktop\Coding\Python\WordSearchWeb\wordsearch\board\word_list.txt', 'r', encoding="mbcs") as f:
             for line in f:
-                clean_line = line = ''.join([char for char in line if char.isalnum() or char.isspace()])
-                ind = clean_line.find(" ")
-                self.word_dic[clean_line[:ind]] = clean_line[ind+1:]
+                self.word_dic[line[:-2]] = 1
 
         self.letters = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 
             14: 'O', 15: 'P', 16: 'Qu', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
